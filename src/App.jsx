daa7ffc2e1b39c851a16f914fc8609adec5c3e7c
nat-laz <lazarev.n@yahoo.com>
2022-08-12 14:46:16 +0200

    start functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+
 import EmojisContainer from "./components/EmojisContainer";
 import Header from "./components/Header";
 
 function App() {
+  
+  
+
   return (
+    
     <div className="App">
       <Header />
       <EmojisContainer />
--- src/components/EmojisContainer.jsx
+++ src/components/EmojisContainer.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
 import emojis from "../data/emoji.json";
 import EmojiItem from "./EmojiItem";
 
 const EmojisContainer = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [emoji, setEmoji] = useState(emojis);
+  const [filteredEmo, setFilteredEmo]= useState([])
+  
+  console.log("Array: ",emoji)
+  console.log("Input: ",inputValue);
+
+  const searchingEmoji = (text) =>{
+      let result = emoji.filter(item=>item.keywords.includes(text))
+      setFilteredEmo(result)
+  }
+
+
+  useEffect(()=>{
+    !inputValue && setEmoji(emojis)
+    searchingEmoji(inputValue)
+  },[inputValue])
+
   return (
     <div className="main">
       <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         className="input"
         type="text"
         name="seach"
         placeholder="search for emoji"
       />
       <div className="emoji-wrapper">
-        {emojis.map((emoji) => {
-          return <EmojiItem symbol={emoji.symbol} />;
+        {emoji.map((item) => {
+          return <EmojiItem key={item.title} symbol={item.symbol} />;
         })}
       </div>
     </div>
